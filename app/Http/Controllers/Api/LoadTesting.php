76acf2dff1138012fc19a8a51ba9aab5f2bc7568
Bagaskara Muhammad Noer <bagaskaramnoer@gmail.com>
2025-06-02 20:39:07 +0700

    add log

--- app/Http/Controllers/Api/LoadTesting.php
+++ app/Http/Controllers/Api/LoadTesting.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 
 class LoadTesting extends Controller
 {
-    public function loadTesting($count)
+    public function loadTesting(Request $request, $count)
     {
-        $resultData = DB::table('linen_lists')
+        try{
+            $resultData = DB::table('linen_lists')
             ->whereNull('linen_lists.deleted_at')
             ->inRandomOrder()
             ->limit($count)
@@ -19,14 +22,25 @@ class LoadTesting extends Controller
             ->leftJoin('linen_types', 'registrations.linen_type_id', '=', 'linen_types.id')
             ->select('epc_lists.tag_id', 'linen_types.linen_type_name')
             ->get();
-
         $data = ([
             'success' => true,
             'code' => 200,
             'message' => 'success get data',
             'epc_lists' => $resultData
         ]);
+        Log::info('success');
 
         return response()->json($data);
+        } catch (\Throwable $th) {
+            $data = ([
+            'success' => false,
+            'code' => 400,
+            'message' => 'error : ' . $th
+        ]);
+        Log::error('gagal');
+
+        return response()->json($data);
+
+        }
     }
 }
